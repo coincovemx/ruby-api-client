caa064a48ddd1afb6aa13ec96def776e0794163f
Genaro Madrid <genaro@appio.mx>
2015-02-10 22:43:32 -0600

    raise error if no token is been initialize

--- lib/volabit/api.rb
+++ lib/volabit/api.rb
@@ -10,21 +10,28 @@ module Volabit
 
     # Request a resource using the GET method.
     def get_resource(resource, params = nil)
+      raise no_token_error unless @token
       @token.refresh if @token.expired?
       response = @token.get(resource, params: params)
       JSON.parse response.body, :symbolize_names => true
     end
 
     def post_to_resource(resource, params)
+      raise no_token_error unless @token
       @token.refresh if @token.expired?
       response = @token.post(resource, params: params)
       JSON.parse response.body, :symbolize_names => true
     end
 
     def delete_resource(resource)
+      raise no_token_error unless @token
       @token.refresh if @token.expired?
       response = @token.delete(resource)
       JSON.parse response.body, :symbolize_names => true
+    end
+
+    def no_token_error
+      'Error: you have to run get_token or set_token before use this method.'
     end
   end
 end
