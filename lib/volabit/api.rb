75d30844dfb30af89062da96f294d2bd1fad6326
Genaro Madrid <genaro@appio.mx>
2015-02-10 21:20:28 -0600

    convert json to hash

--- lib/volabit/api.rb
+++ lib/volabit/api.rb
@@ -12,19 +12,19 @@ module Volabit
     def get_resource(resource, params = nil)
       @token.refresh if @token.expired?
       response = @token.get(resource, params: params)
-      JSON.load response.body
+      JSON.parse response.body, :symbolize_names => true
     end
 
     def post_to_resource(resource, params)
       @token.refresh if @token.expired?
       response = @token.post(resource, params: params)
-      JSON.load response.body
+      JSON.parse response.body, :symbolize_names => true
     end
 
     def delete_resource(resource)
       @token.refresh if @token.expired?
       response = @token.delete(resource)
-      JSON.load response.body
+      JSON.parse response.body, :symbolize_names => true
     end
   end
 end
