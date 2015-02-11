c1f13f76a8986e0d1fefb950133a13c99e77d8ea
Genaro Madrid <genaro@appio.mx>
2015-02-10 20:44:01 -0600

    always return json

--- lib/volabit/api.rb
+++ lib/volabit/api.rb
@@ -11,17 +11,20 @@ module Volabit
     # Request a resource using the GET method.
     def get_resource(resource, params = nil)
       @token.refresh if @token.expired?
-      @token.get(resource, params: params)
+      response = @token.get(resource, params: params)
+      JSON.load response.body
     end
 
     def post_to_resource(resource, params)
       @token.refresh if @token.expired?
-      @token.post(resource, params: params)
+      response = @token.post(resource, params: params)
+      JSON.load response.body
     end
 
     def delete_resource(resource)
       @token.refresh if @token.expired?
-      @token.delete(resource)
+      response = @token.delete(resource)
+      JSON.load response.body
     end
   end
 end
